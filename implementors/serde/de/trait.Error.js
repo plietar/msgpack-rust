ad1c238c153d32444cb3e99bcbd0030dafee169d
Evgeny Safronov <division494@gmail.com>
2015-08-10 23:13:32 +0300

    [GHP] Rebuild docs at 774e994.

---
+++ implementors/serde/de/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rmp'] = ["impl <a class='trait' href='https://serde-rs.github.io/serde/serde/serde/de/trait.Error.html' title='serde::de::Error'>Error</a> for <a class='enum' href='rmp/decode/serde/enum.Error.html' title='rmp::decode::serde::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
