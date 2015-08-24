13fcc1cfaf977864358ae6ac8acbd5fe897de822
Evgeny Safronov <division494@gmail.com>
2015-08-24 14:42:30 +0300

    [GHP] Rebuild docs at 2b9d2b9.

---
+++ rmp-serde/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rmp_serde'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;FixedValueReadError&gt; for <a class='enum' href='rmp_serde/decode/enum.Error.html' title='rmp_serde::decode::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;ValueReadError&gt; for <a class='enum' href='rmp_serde/decode/enum.Error.html' title='rmp_serde::decode::Error'>Error</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;DecodeStringError&lt;'a&gt;&gt; for <a class='enum' href='rmp_serde/decode/enum.Error.html' title='rmp_serde::decode::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;MarkerReadError&gt; for <a class='enum' href='rmp_serde/decode/enum.Error.html' title='rmp_serde::decode::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;FixedValueWriteError&gt; for <a class='enum' href='rmp_serde/encode/enum.Error.html' title='rmp_serde::encode::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;ValueWriteError&gt; for <a class='enum' href='rmp_serde/encode/enum.Error.html' title='rmp_serde::encode::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ rmp-serialize/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rmp_serialize'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;FixedValueReadError&gt; for <a class='enum' href='rmp_serialize/decode/enum.Error.html' title='rmp_serialize::decode::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;ValueReadError&gt; for <a class='enum' href='rmp_serialize/decode/enum.Error.html' title='rmp_serialize::decode::Error'>Error</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;DecodeStringError&lt;'a&gt;&gt; for <a class='enum' href='rmp_serialize/decode/enum.Error.html' title='rmp_serialize::decode::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;FixedValueWriteError&gt; for <a class='enum' href='rmp_serialize/encode/enum.Error.html' title='rmp_serialize::encode::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;ValueWriteError&gt; for <a class='enum' href='rmp_serialize/encode/enum.Error.html' title='rmp_serialize::encode::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
